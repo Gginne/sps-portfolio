a6807a7059e6cde4c7a430b4ee2e594c453082ba
Gginne <gginne.github.io>
2021-03-08 22:24:35 +0000

    Created contact servlet

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -49,4 +49,39 @@ projectContainer.innerHTML = projectContent
     console.err(err)
 })
 
-
+/*
+const projects = [
+    {
+        name: "Store202",
+        description: "Online shoe store built in php",
+        github: "https://github.com/Gginne/IT202/tree/master/project",
+        host: "https://web.njit.edu/~gc348/IT202/project"
+    },
+    {
+        name: "Photopress",
+        description: "Photo-saving app built on mern stack",
+        github: "https://github.com/Gginne/photopress",
+        host: "http://photopress.herokuapp.com/"
+    },
+      {
+        name: "Mixionary",
+        description: "Dictionary web app",
+        github: "https://github.com/Gginne/mixionary",
+        host: "https://gginne.github.io/mixionary/"
+    },
+     {
+        name: "WeatherMe",
+        description: "Weather & forecast web app",
+        github: "https://github.com/Gginne/weather-app",
+        host: "https://weatherandme.netlify.app/"
+    },
+   
+    {
+        name: "Expense tracker",
+        description: "Expense tracking web app built in vanilla js",
+        github: "https://github.com/Gginne/expense-tracker",
+        host: "https://gginne.github.io/expense-tracker/"
+    }
+ 
+  
+]*/
